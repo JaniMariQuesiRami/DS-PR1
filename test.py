d6b4d1bde5d417753c16e7ec3c6b5a5260cfbe1f
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2024-08-11 15:19:38 -0600

    feat: load and merge and clean easy columns

---
+++ test.py
@@ -0,0 +1,7 @@
+# Importamos librerias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
