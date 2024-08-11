95181edb207258cc5c732518cd3f8ae097f5a6c3
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2024-08-11 17:00:27 -0600

    fix: remove unused file

--- test.py
+++
@@ -1,7 +0,0 @@
-# Importamos librerias
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from ydata_profiling import ProfileReport
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
